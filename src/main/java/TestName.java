beff1dc1909ea549b48d01d814915d10f228f0ce
Perugu ReenaRoy Sree <perugu.reenaroysree@publicissapient.com>
2022-04-05 17:01:19 +0530

    assesment by reena

---
+++ src/main/java/TestName.java
@@ -0,0 +1,10 @@
+
+public class TestName {
+
+	public static void main(String[] args) {
+		String firstname="Reena";
+		String lastname="Roy";
+		System.out.println("Hello "+firstname + ""+lastname);
+	}
+
+}
